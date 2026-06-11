a39a0163278fc517ea830f1963400003115a2a49
Abdul Azees P R <abdulazeespr@gmail.com>
2026-06-11 16:36:37 +0530

    add:social image

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,10 @@ export const metadata: Metadata = {
   title: "Fayez & Hasna Wedding Invitation",
   description: "You are cordially invited to the reception ceremony of Fayez & Hasna.",
   openGraph: {
+    title: "Fayez & Hasna Wedding Invitation",
+    description: "You are cordially invited to the reception ceremony of Fayez & Hasna.",
     images: ["@file:social_image.jpg"],
+    url: "https://wedcard-puce.vercel.app/social_image.jpg",
   },
 };
 
